25dbc398d97386cd225cbb2c3b4a4a569a9f2d3c
Victor Ivan <viictor.ivan@gmail.com>
2025-05-23 00:38:35 +0000

    adding bunny envs default

--- src/common/env/index.ts
+++ src/common/env/index.ts
@@ -9,11 +9,10 @@ const envSchema = z.object({
 	RABBITMQ_QUEUE: z.string().default("images.process"),
 	RABBITMQ_EXCHANGE: z.string().default("images.process"),
 	RABBITMQ_RETRIES_TRESHOLD: z.number().default(5),
-	STORAGE_TYPE: z.enum(["local", "bunny"]).default("local"),
-
-	BUNNY_STORAGE_NAME: z.string().optional(),
-	BUNNY_STORAGE_API_KEY: z.string().optional(),
-	BUNNY_STORAGE_HOSTNAME: z.string().optional(),
+	STORAGE_TYPE: z.enum(["local", "bunny"]).default("bunny"),
+	BUNNY_STORAGE_NAME: z.string().optional().default("victor-cdn-storage"),
+	BUNNY_STORAGE_API_KEY: z.string().optional().default("2f3d5a4b-2f38-48f2-8db3fc18c168-4c49-4ee3"),
+	BUNNY_STORAGE_HOSTNAME: z.string().optional().default("br.storage.bunnycdn.com"),
 });
 
 const envs = envSchema.parse(process.env);
